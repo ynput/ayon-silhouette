0c6a9344cb5663d09944c8aca3cccd17c09c4205
Roy Nieterau <roy_nieterau@hotmail.com>
2025-01-08 10:00:23 +0100

    Fix tabs

--- client/ayon_silhouette/startup/__init__.py
+++ client/ayon_silhouette/startup/__init__.py
@@ -13,46 +13,46 @@ install_host(SilhouetteHost())
 
 
 def _add_menu_deferred():
-	parent = get_main_window()
+    parent = get_main_window()
 
-	menu_label = os.environ.get("AYON_MENU_LABEL") or "AYON"
-	menu = parent.menuBar().addMenu(menu_label)
+    menu_label = os.environ.get("AYON_MENU_LABEL") or "AYON"
+    menu = parent.menuBar().addMenu(menu_label)
 
-	# TODO: Add current context label menu entry
-	# TODO: Add version up menu entry
+    # TODO: Add current context label menu entry
+    # TODO: Add version up menu entry
 
-	action = menu.addAction("Create...")
-	action.triggered.connect(
-		lambda: host_tools.show_publisher(parent=parent,
-										  tab="create")
-	)
+    action = menu.addAction("Create...")
+    action.triggered.connect(
+        lambda: host_tools.show_publisher(parent=parent,
+                                          tab="create")
+    )
 
-	action = menu.addAction("Load...")
-	action.triggered.connect(
-		lambda: host_tools.show_loader(parent=parent, use_context=True)
-	)
+    action = menu.addAction("Load...")
+    action.triggered.connect(
+        lambda: host_tools.show_loader(parent=parent, use_context=True)
+    )
 
-	action = menu.addAction("Publish...")
-	action.triggered.connect(
-		lambda: host_tools.show_publisher(parent=parent,
-										  tab="publish")
-	)
+    action = menu.addAction("Publish...")
+    action.triggered.connect(
+        lambda: host_tools.show_publisher(parent=parent,
+                                          tab="publish")
+    )
 
-	action = menu.addAction("Manage...")
-	action.triggered.connect(
-		lambda: host_tools.show_scene_inventory(parent=parent)
-	)
+    action = menu.addAction("Manage...")
+    action.triggered.connect(
+        lambda: host_tools.show_scene_inventory(parent=parent)
+    )
 
-	action = menu.addAction("Library...")
-	action.triggered.connect(
-		lambda: host_tools.show_library_loader(parent=parent)
-	)
+    action = menu.addAction("Library...")
+    action.triggered.connect(
+        lambda: host_tools.show_library_loader(parent=parent)
+    )
 
-	menu.addSeparator()
-	action = menu.addAction("Work Files...")
-	action.triggered.connect(
-		lambda: host_tools.show_workfiles(parent=parent)
-	)
+    menu.addSeparator()
+    action = menu.addAction("Work Files...")
+    action.triggered.connect(
+        lambda: host_tools.show_workfiles(parent=parent)
+    )
 
 
 QtCore.QTimer.singleShot(0, _add_menu_deferred)
