e381dd748ccb1ab05683335a4e40fdea5138c4de
Roy Nieterau <roy_nieterau@hotmail.com>
2025-01-08 12:08:03 +0100

    Add extra menu entries

--- client/ayon_silhouette/api/pipeline.py
+++ client/ayon_silhouette/api/pipeline.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import contextlib
+from functools import partial
 
 import pyblish.api
 from qtpy import QtCore
@@ -11,7 +12,10 @@ from ayon_core.pipeline import (
     register_loader_plugin_path,
     register_creator_plugin_path,
     AYON_CONTAINER_ID,
+    get_current_context,
 )
+from ayon_core.pipeline.context_tools import version_up_current_workfile
+from ayon_core.settings import get_current_project_settings
 from .workio import (
     open_file,
     save_file,
@@ -61,13 +65,36 @@ class SilhouetteHost(HostBase, IWorkfileHost, ILoadHost, IPublishHost):
         defer(self._install_menu)
 
     def _install_menu(self):
+        project_settings = get_current_project_settings()
         parent = lib.get_main_window()
 
         menu_label = os.environ.get("AYON_MENU_LABEL") or "AYON"
         menu = parent.menuBar().addMenu(menu_label)
 
-        # TODO: Add current context label menu entry
-        # TODO: Add version up menu entry
+        # Add current context label
+        def _set_current_context_label(action):
+            context = get_current_context()
+            label = "{0[folder_path]}, {0[task_name]}".format(context)
+            action.setText(label)
+
+        action = menu.addAction("Current Context")
+        action.setEnabled(False)
+
+        # Update context label on menu show
+        menu.aboutToShow.connect(partial(_set_current_context_label, action))
+
+        menu.addSeparator()
+
+        # Add Version Up Workfile menu entry
+        try:
+            if project_settings["core"]["tools"]["ayon_menu"].get(
+                "version_up_current_workfile"):
+                    action = menu.addAction("Version Up Workfile")
+                    action.triggered.connect(version_up_current_workfile)
+                    menu.addSeparator()
+        except KeyError:
+            print("Version Up Workfile setting not found in "
+                  "Core Settings. Please update Core Addon")
 
         action = menu.addAction("Create...")
         action.triggered.connect(
