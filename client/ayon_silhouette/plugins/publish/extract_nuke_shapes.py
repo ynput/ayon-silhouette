0e6513ae56af126124609a9772e50bad3a0dfeec
Roy Nieterau <roy_nieterau@hotmail.com>
2025-01-10 00:52:11 +0100

    Implement more Track Points extractors (prototype)

--- client/ayon_silhouette/plugins/publish/extract_nuke_shapes.py
+++ client/ayon_silhouette/plugins/publish/extract_nuke_shapes.py
@@ -13,11 +13,14 @@ class ExtractNukeShapes(publish.Extractor):
     hosts = ["silhouette"]
     families = ["trackpoints", "matteshapes"]
 
+    extension = "nk"
+    io_module = "Nuke 9+ Shapes"
+
     def process(self, instance):
 
         # Define extract output file path
         dir_path = self.staging_dir(instance)
-        filename = "{0}.nk".format(instance.name)
+        filename = "{0}.{1}".format(instance.name, self.extension)
         path = os.path.join(dir_path, filename)
 
         node = instance.data["transientData"]["instance_node"]
@@ -25,14 +28,36 @@ class ExtractNukeShapes(publish.Extractor):
 
         with lib.maintained_selection():
             fx.select(shapes)
-            fx.io_modules["Nuke 9+ Shapes"].export(path)
+            fx.io_modules[self.io_module].export(path)
 
         representation = {
-            "name": "nk",
-            "ext": "nk",
+            "name": self.extension,
+            "ext": self.extension,
             "files": filename,
             "stagingDir": dir_path,
         }
         instance.data.setdefault("representations", []).append(representation)
 
         self.log.info(f"Extracted instance '{instance.name}' to: {path}")
+
+
+class ExtractFusionShapes(ExtractNukeShapes):
+    """Extract node as Fusion Shapes."""
+    # TODO: Suppress a pop-up dialog
+    label = "Extract Fusion Shapes"
+    extension = "setting"
+    io_module = "Fusion Shapes"
+
+
+class ExtractSilhouetteShapes(ExtractNukeShapes):
+    """Extract node as Silhouette Shapes."""
+    label = "Extract Silhouette Shapes"
+    extension = "fxs"
+    io_module = "Silhouette Shapes"
+
+
+class ExtractShakeShapes(ExtractNukeShapes):
+    """Extract node as Shake Shapes."""
+    label = "Extract Shape Shapes"
+    extension = "ssf"
+    io_module = "Shake 4.x SSF"
