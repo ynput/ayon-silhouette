923934fc0999f96e4094e57b6b6a4b5fe5a349e9
Roy Nieterau <roy_nieterau@hotmail.com>
2025-01-15 13:36:39 +0100

    Initial support for local render

--- client/ayon_silhouette/plugins/publish/extract_render.py
+++ client/ayon_silhouette/plugins/publish/extract_render.py
@@ -13,13 +13,8 @@ class SilhouetteExtractRender(publish.Extractor):
     def process(self, instance):
 
         # Collect the start and end including handles
-        # start = instance.data["frameStartHandle"]
-        # end = instance.data["frameEndHandle"]
-
-        # Define extract output file path
-        dir_path = self.staging_dir(instance)
-        filename = "{0}.mp4".format(instance.name)
-        path = os.path.join(dir_path, filename)
+        start = instance.data["frameStartHandle"]
+        end = instance.data["frameEndHandle"]
 
         # TODO: Implement
         output_node = instance.data["transientData"]["instance_node"]
@@ -30,7 +25,7 @@ class SilhouetteExtractRender(publish.Extractor):
         # progress = PreviewProgressHandler()
         # progress.preview = True
         # progress.pixelAspect = session.pixelAspect
-        renderer.render({
+        finished = renderer.render({
                 "session": session,
                 "nodes": [output_node],
                 # Override frame range
@@ -39,13 +34,45 @@ class SilhouetteExtractRender(publish.Extractor):
             # progress=progress
         )
 
-        # TODO: Get correct extension
+        if not finished:
+            raise publish.PublishError("Render was cancelled or interrupted.")
+
+        outputs = renderer.outputs
+        if not outputs:
+            raise publish.PublishError("Render generated no outputs.")
+
+        # Collect all rendered outputs
+        filepaths = []
+        for output in outputs:
+            for frame in range(start, end+1):
+                filepath = output.buildPath(frame)
+                filepaths.append(filepath)
+
+        # All files must exist. A rendered output may not exist due to
+        # unexpected failures, or if the work range is smaller than the render
+        # range.
+        # TODO: Validate to handle render range out of work range better
+        for filepath in filepaths:
+            if not os.path.exists(filepath):
+                raise publish.PublishError(f"File does not exist: {filepath}")
+
+        # For now assume one output sequence per instance
+        first_filepath = filepaths[0]
+        files = [os.path.basename(path) for path in filepaths]
+        staging_dir = os.path.dirname(first_filepath)
+        ext = os.path.splitext(first_filepath)[-1]
+
+        # Workaround: Single files must not be a list
+        if len(files) == 1:
+            files = files[0]
+
         representation = {
-            "name": "mp4",
-            "ext": "mp4",
-            "files": filename,
-            "stagingDir": dir_path,
+            "name": ext.lstrip("."),
+            "ext": ext.lstrip("."),
+            "files": files,
+            "stagingDir": staging_dir,
         }
         instance.data.setdefault("representations", []).append(representation)
 
-        self.log.info(f"Extracted instance '{instance.name}' to: {path}")
+        self.log.debug(
+            f"Extracted instance '{instance.name}' to: {filepaths[0]}")
