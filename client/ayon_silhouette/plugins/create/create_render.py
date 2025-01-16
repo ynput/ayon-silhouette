1049a540216ad7798dd16fccf8a358b3a3564406
Roy Nieterau <roy_nieterau@hotmail.com>
2025-01-16 13:40:04 +0100

    Set a default render path

--- client/ayon_silhouette/plugins/create/create_render.py
+++ client/ayon_silhouette/plugins/create/create_render.py
@@ -13,7 +13,19 @@ class CreateRender(plugin.SilhouetteCreator):
     product_type = "render"
     icon = "eye"
 
-    # TODO: Set default output filepath?
+    def create(self, product_name, instance_data, pre_create_data):
+
+        with lib.undo_chunk("Create Render"):
+            instance = super().create(
+                product_name, instance_data, pre_create_data)
+
+            # Set default render output path
+            # TODO: Make this configurable in settings
+            instance_node = instance.transient_data["instance_node"]
+            instance_node.path.value = (
+                "$(AYON_WORKDIR)/renders/silhouette/"
+                f"{product_name}/{product_name}"
+            )
 
     def get_instance_attr_defs(self):
         defs = lib.collect_animation_defs(self.create_context)
