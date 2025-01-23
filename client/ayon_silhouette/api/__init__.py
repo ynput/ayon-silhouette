a70f364b518dc8972c3d99fbfed6296c185313f7
Roy Nieterau <roy_nieterau@hotmail.com>
2025-01-23 15:04:42 +0100

    Remove removed `workio`

--- client/ayon_silhouette/api/__init__.py
+++ client/ayon_silhouette/api/__init__.py
@@ -6,16 +6,7 @@ from .lib import (
     maintained_selection
 )
 
-from .workio import (
-    save_file,
-    current_file,
-    has_unsaved_changes
-)
-
 __all__ = [
     "SilhouetteHost",
     "maintained_selection",
-    "save_file",
-    "current_file",
-    "has_unsaved_changes",
 ]
