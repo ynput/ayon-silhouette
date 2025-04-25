be2db3f920b9220555e17278b08d19bee7830bc4
Roy Nieterau <roy_nieterau@hotmail.com>
2025-04-25 16:10:14 +0200

    Revert invalid change

--- client/ayon_silhouette/plugins/publish/extract_shapes.py
+++ client/ayon_silhouette/plugins/publish/extract_shapes.py
@@ -60,7 +60,6 @@ class ExtractNukeShapes(publish.Extractor,
             ]
 
         with lib.maintained_selection():
-            fx.activate(node)
             fx.select(shapes)
             with contextlib.ExitStack() as stack:
                 self.log.debug(f"Exporting '{self.io_module}' to: {path}")
